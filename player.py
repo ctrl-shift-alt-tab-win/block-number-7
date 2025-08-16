1fe925a9b6a12e19da4c4592cb28174a784e2f9c
Qizheng Xie <jx1824@ic.ac.uk>
2025-08-16 21:24:30 +0300

    Implemented buying and upgrading of property

--- player.py
+++ player.py
@@ -4,7 +4,28 @@ class Player:
         self.name = name
         self.cash = cash_initial
         self.position = 0
-        self.property_list = []
 
     def move(self, steps):
         self.position = (self.position + steps) % 28
+
+    def buy_property(self, target_property):
+        if self.cash < target_property.price:
+            print(f"Not enough money to buy {target_property.name}...")
+        else:
+            print(f"Successfully bought {target_property.name}!")
+            self.cash -= target_property.price
+            target_property.owner_id = self.player_id
+            target_property.level = 1
+
+    def upgrade_property(self, target_property):
+        if target_property.level == 3:
+            print(f"{target_property.name} is already at max level!")
+        elif self.cash < target_property.price:
+            print(f"Not enough money to upgrade {target_property.name}...")
+        else:
+            print(f"Successfully upgraded {target_property.name}!")
+            self.cash -= target_property.price
+            target_property.level += 1
+            target_property.rent *= 2
+
+
