3e46eb0f4ab7773940c7157374357e64bf8409f7
Qizheng Xie <jx1824@ic.ac.uk>
2025-08-17 11:53:13 +0300

    Better messages in player.py

--- player.py
+++ player.py
@@ -13,26 +13,35 @@ class Player:
         print(f"The dice landed on {steps}!")
         self.position = (self.position + steps) % 28
         print(f"Your current position is {self.position}!")
+        input("Press ENTER to continue.")
 
     def buy_property(self, target_property):
         if self.cash < target_property.price:
             print(f"Not enough money to buy {target_property.name}...")
+            input("Press ENTER to continue.")
         else:
             print(f"Successfully bought {target_property.name}!")
             self.cash -= target_property.price
+            print(f"Your cash is now {self.cash}.")
             target_property.owner_id = self.player_id
             target_property.level = 1
+            input("Press ENTER to continue.")
 
     def upgrade_property(self, target_property):
         if target_property.level == 3:
             print(f"{target_property.name} is already at max level!")
+            input("Press ENTER to continue.")
         elif self.cash < target_property.price:
             print(f"Not enough money to upgrade {target_property.name}...")
+            input("Press ENTER to continue.")
         else:
             print(f"Successfully upgraded {target_property.name}!")
             self.cash -= target_property.price
+            print(f"Your cash is now {self.cash}.")
             target_property.level += 1
             target_property.rent *= 2
+            print(f"{target_property.name} is now at level {target_property.level} and the rent is now {target_property.rent}.")
+            input("Press ENTER to continue.")
 
     def pay_rent(self, target_property, target_player):
         if self.cash < target_property.rent:
@@ -43,5 +52,8 @@ class Player:
         else:
             print(f"Successfully paid ${target_property.rent} for {target_property.name}...")
             self.cash -= target_property.rent
-            target_player.rent += target_property.rent
+            target_player.cash += target_property.rent
+            print(f"Your cash is now {self.cash}.")
+            print(f"{target_player.name}'s cash is now {target_player.cash}.")
+            input("Press ENTER to continue.")
             return True
