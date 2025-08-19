0307f490828ed0aac819e83a2ba31c4ae495aed9
Qizheng Xie <jx1824@ic.ac.uk>
2025-08-19 12:11:32 +0300

    Moved all UI code of player.py into game.py

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+import sys
+
 import board
 from player import Player
 
@@ -14,7 +16,10 @@ def game_step(player):
     print(f"-------{player.name}'s turn-------")
     print(f"You are at position {player.position}.")
     input("Press ENTER to roll the dice!\n")
-    player.move()
+    steps, position = player.move()
+    print(f"The dice landed on {steps}!")
+    print(f"Your current position is {position}!")
+    input("Press ENTER to continue.\n")
     accessible_items = game_board.item_list[player.position]
     print(f"You currently border:")
     for item in accessible_items:
@@ -29,7 +34,13 @@ def game_step(player):
                 print(f"Cash needed to purchase {item.name}: {item.price}")
                 choice = input(f"Would you like to buy {item.name}? (Y/N): ")
                 if choice == "Y":
-                    player.buy_property(item)
+                    if player.buy_property(item):
+                        print(f"Successfully bought {item.name}!")
+                        print(f"Your cash is now {player.cash}.")
+                        input("Press ENTER to continue.\n")
+                    else:
+                        print(f"Not enough money to buy {item.name}...")
+                        input("Press ENTER to continue.\n")
                 else:
                     input("Alright. Press ENTER to continue.\n")
             elif item.owner_id == player.player_id:
@@ -37,14 +48,36 @@ def game_step(player):
                 print(f"{item.name} is at level {item.level}.")
                 choice = input(f"Would you like to upgrade {item.name}? (Y/N): ")
                 if choice == "Y":
-                    player.upgrade_property(item)
+                    status = player.upgrade_property(item)
+                    if status == 1:
+                        print(f"Successfully upgraded {item.name}!")
+                        print(f"Your cash is now {player.cash}.")
+                        print(f"{item.name} is now at level {item.level} and the rent is now {item.rent}.")
+                        input("Press ENTER to continue.\n")
+                    elif status == 0:
+                        print(f"{item.name} is already at max level!")
+                        input("Press ENTER to continue.\n")
+                    elif status == -1:
+                        print(f"Not enough money to upgrade {item.name}...")
+                        input("Press ENTER to continue.\n")
                 else:
                     input("Alright. Press ENTER to continue.\n")
             else:
                 print(f"Oops, {item.name} is owned by {players_list[item.owner_id-1].name}.")
                 print(f"You need to pay rent of {item.rent}.")
                 input("Press ENTER to pay rent.\n")
-                player.pay_rent(item, players_list[item.owner_id-1])
+                target_player = players_list[item.owner_id-1]
+                if player.pay_rent(item, target_player):
+                    print(f"Successfully paid ${item.rent} for {item.name}...")
+                    print(f"Your cash is now {player.cash}.")
+                    print(f"{target_player.name}'s cash is now {target_player.cash}.")
+                    input("Press ENTER to continue.\n")
+                else:
+                    print(f"Not enough money to pay rent for {item.name}...")
+                    print(f"{target_player.name} WON!!!")
+                    input("Press ENTER to exit.\n")
+                    sys.exit()
+                    # TODO: Generalise to more than 2 players
 
         elif isinstance(item, board.Tower):
             input("Feature currently not available. Press ENTER to continue.\n")
--- player.py
+++ player.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 
 class Player:
@@ -11,50 +10,33 @@ class Player:
 
     def move(self):
         steps = random.randint(1, 6)
-        print(f"The dice landed on {steps}!")
         self.position = (self.position + steps) % 28
-        print(f"Your current position is {self.position}!")
-        input("Press ENTER to continue.\n")
+        return steps, self.position
 
     def buy_property(self, target_property):
         if self.cash < target_property.price:
-            print(f"Not enough money to buy {target_property.name}...")
-            input("Press ENTER to continue.\n")
+            return False
         else:
-            print(f"Successfully bought {target_property.name}!")
             self.cash -= target_property.price
-            print(f"Your cash is now {self.cash}.")
             target_property.owner_id = self.player_id
             target_property.level = 1
-            input("Press ENTER to continue.\n")
+            return True
 
     def upgrade_property(self, target_property):
         if target_property.level == 3:
-            print(f"{target_property.name} is already at max level!")
-            input("Press ENTER to continue.\n")
+            return 0
         elif self.cash < target_property.price:
-            print(f"Not enough money to upgrade {target_property.name}...")
-            input("Press ENTER to continue.\n")
+            return -1
         else:
-            print(f"Successfully upgraded {target_property.name}!")
             self.cash -= target_property.price
-            print(f"Your cash is now {self.cash}.")
             target_property.level += 1
             target_property.rent *= 2
-            print(f"{target_property.name} is now at level {target_property.level} and the rent is now {target_property.rent}.")
-            input("Press ENTER to continue.\n")
+            return 1
 
     def pay_rent(self, target_property, target_player):
         if self.cash < target_property.rent:
-            print(f"Not enough money to pay rent for {target_property.name}...")
-            print(f"{target_player.name} WON!!!")
-            input("Press ENTER to exit.\n")
-            sys.exit()
-            #TODO: Generalise to more than 2 players
+            return False
         else:
-            print(f"Successfully paid ${target_property.rent} for {target_property.name}...")
             self.cash -= target_property.rent
             target_player.cash += target_property.rent
-            print(f"Your cash is now {self.cash}.")
-            print(f"{target_player.name}'s cash is now {target_player.cash}.")
-            input("Press ENTER to continue.\n")
+            return True
