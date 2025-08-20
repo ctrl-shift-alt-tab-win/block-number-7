8451575d73f48a0b319873ecc6d21a0d52221314
Qizheng Xie <jx1824@ic.ac.uk>
2025-08-20 19:55:59 +0300

    Break game_step() into smaller methods

--- game.py
+++ game.py
@@ -12,6 +12,42 @@ def game_start_routine():
     return players
 
 
+def game_handle_property(item, player):
+    if item.owner_id == 0:
+        choice = ui.property.ask_buy_property(item, player)
+        if choice == "Y":
+            if player.buy_property(item):
+                ui.property.buy_property_success(item, player)
+                if game_board.check_for_complete_group(item.group, player):
+                    ui.property.player_own_complete_group(item.group)
+            else:
+                ui.property.buy_property_failure(item)
+        else:
+            ui.property.player_refuse_buy_property()
+    elif item.owner_id == player.player_id:
+        choice = ui.property.ask_upgrade_property(item, player)
+        if choice == "Y":
+            status = player.upgrade_property(item)
+            if status == 1:
+                ui.property.upgrade_property_success(item, player)
+            elif status == 0:
+                ui.property.upgrade_property_failure_max_level(item)
+            elif status == -1:
+                ui.property.upgrade_property_failure_money(item)
+        else:
+            ui.property.player_refuse_upgrade_property()
+    else:
+        target_player = players_list[item.owner_id - 1]
+        ui.property.need_to_pay_rent(item, target_player)
+        if player.pay_rent(item, target_player):
+            ui.property.pay_rent_success(item, player, target_player)
+        else:
+            ui.property.pay_rent_failure(item)
+            ui.game.won(target_player)
+            sys.exit()
+            # TODO: Generalise to more than 2 players
+
+
 def game_step(player):
     ui.game.turn_start_roll_dice(player)
     steps, position = player.move()
@@ -21,76 +57,34 @@ def game_step(player):
     for item in accessible_items:
         ui.game.consider_an_item(item)
         if isinstance(item, Property):
-            if item.owner_id == 0:
-                choice = ui.property.ask_buy_property(item, player)
-                if choice == "Y":
-                    if player.buy_property(item):
-                        ui.property.buy_property_success(item, player)
-                        if game_board.check_for_complete_group(item.group, player):
-                            ui.property.player_own_complete_group(item.group)
-                    else:
-                        ui.property.buy_property_failure(item)
-                else:
-                    ui.property.player_refuse_buy_property()
-            elif item.owner_id == player.player_id:
-                choice = ui.property.ask_upgrade_property(item, player)
-                if choice == "Y":
-                    status = player.upgrade_property(item)
-                    if status == 1:
-                        ui.property.upgrade_property_success(item, player)
-                    elif status == 0:
-                        ui.property.upgrade_property_failure_max_level(item)
-                    elif status == -1:
-                        ui.property.upgrade_property_failure_money(item)
-                else:
-                    ui.property.player_refuse_upgrade_property()
-            else:
-                target_player = players_list[item.owner_id - 1]
-                ui.property.need_to_pay_rent(item, target_player)
-                if player.pay_rent(item, target_player):
-                    ui.property.pay_rent_success(item, player, target_player)
-                else:
-                    ui.property.pay_rent_failure(item)
-                    ui.game.won(target_player)
-                    sys.exit()
-                    # TODO: Generalise to more than 2 players
-
+            game_handle_property(item, player)
         elif isinstance(item, Tower):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, GoodChest):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, BadChest):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, Hotel):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, Park):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, Pond):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, Bank):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, Supermarket):
             ui.game.feature_not_available()
             #TODO
-
         elif isinstance(item, Restaurant):
             ui.game.feature_not_available()
             #TODO
-
 
 game_board = Board()
 ui = ConsoleUI()
